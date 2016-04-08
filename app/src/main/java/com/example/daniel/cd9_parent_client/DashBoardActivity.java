007c6a8d01287e56bb794e6ddfc5d1d8a5c0cb7f
drealnn <drealnn@gmail.com>
2016-04-08 01:36:25 -0400

    Finished networking in loginActivity. Registers GCM token and successfully sends token to server using credentials. Opens dashboard if the reg token was successfully sent.

---
+++ app/src/main/java/com/example/daniel/cd9_parent_client/DashBoardActivity.java
@@ -0,0 +1,31 @@
+package com.example.daniel.cd9_parent_client;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class DashBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash_board);
+
+
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://209.208.28.59:8000/CD9/login");
+    }
+
+}
